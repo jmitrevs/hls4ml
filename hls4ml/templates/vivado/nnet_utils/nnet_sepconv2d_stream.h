69ac0481b6b9ee4d69fe634c85172f5326fed577
ChiRui Chen <kugelblitz.ee05@gmail.com>
2022-09-16 17:49:18 +0800

    add depthwise_conv_2d_ss

--- hls4ml/templates/vivado/nnet_utils/nnet_sepconv2d_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_sepconv2d_stream.h
@@ -128,5 +128,161 @@ void separable_conv_2d_cl(
     pointwise_conv_2d_cl<data_T, res_T, typename CONFIG_T::pointwise_config>(depthwise_res, res, pointwise_weights, pointwise_biases);
 }
 
+//Single stream for Depthwise Conv2d
+
+template<class data_T, class res_T, typename CONFIG_T>
+void depthwise_ss_product(
+    data_T    data[CONFIG_T::n_in],
+    res_T     res[CONFIG_T::n_out],
+    typename CONFIG_T::weight_t  weights[CONFIG_T::n_in*CONFIG_T::n_out],
+    typename CONFIG_T::bias_t biases[CONFIG_T::n_out]
+) {
+    #pragma HLS INLINE
+
+    typename CONFIG_T::accum_t mult[CONFIG_T::n_in];
+    typename CONFIG_T::accum_t acc[CONFIG_T::n_out];
+
+    // Use a function_instantiate in case it helps to explicitly optimize unchanging weights/biases
+    #pragma HLS function_instantiate variable=weights
+
+    #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
+
+    #pragma HLS ARRAY_PARTITION variable=mult complete
+
+    //int multiplier_limit  = ceil(float(CONFIG_T::kernel_size * CONFIG_T::n_chan) / float(CONFIG_T::reuse_factor)) - floor(float(CONFIG_T::n_zeros) / float(CONFIG_T::reuse_factor));
+	
+	const int multfactor = MIN(CONFIG_T::n_in,CONFIG_T::reuse_factor);
+    const int multiplier_limit = DIV_ROUNDUP(CONFIG_T::n_in*CONFIG_T::n_out, multfactor);
+	
+    CONFIG_T::template product<data_T, typename CONFIG_T::weight_t>::limit(multiplier_limit);
+
+    // Do the matrix-multiply
+    Product: for(int ii = 0; ii < CONFIG_T::n_in; ii++) {
+        #pragma HLS UNROLL
+        mult[ii] = CONFIG_T::template product<data_T, typename CONFIG_T::weight_t>::product(data[ii], weights[ii]);
+    }
+
+    // Initialize accumulator with input biases
+    ResetAccum: for(int iacc = 0; iacc < CONFIG_T::n_out; iacc++) {
+        #pragma HLS UNROLL
+        acc[iacc] = (typename CONFIG_T::accum_t) biases[iacc];
+    }
+
+    // Accumulate multiplication result
+	
+	const int kernel_size = CONFIG_T::n_in / CONFIG_T::n_out;
+    Accum1: for(int ii = 0; ii < kernel_size; ii++) {
+        Accum2: for(int jj = 0; jj < CONFIG_T::n_out; jj++) {
+            int index = ii * CONFIG_T::n_out + jj;
+            acc[jj] += mult[index];
+        }
+    }
+
+    // Cast to "res_t" type
+    // Result: for(int ires = 0; ires < CONFIG_T::n_chan; ires++){
+        // #pragma HLS UNROLL
+        // res[ires] = cast<data_T, res_T, CONFIG_T>(acc[ires]);
+    // }
+	for (int ires = 0; ires < CONFIG_T::n_out; ires++) {
+        #pragma HLS UNROLL
+        //res[ires] = cast<data_T, res_T, CONFIG_T>(acc[ires]);
+        res_T tmp = acc[ires];
+        res[ires] =  tmp;
+    }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
+void depthwise_conv_2d_ss_cl(
+    hls::stream<data_T> &data,
+    hls::stream<res_T>  &res,
+    typename CONFIG_T::weight_t weights[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_filt],
+    typename CONFIG_T::bias_t   biases[CONFIG_T::n_filt]){
+    
+    assert(CONFIG_T::pad_top == 0 && CONFIG_T::pad_bottom == 0 && CONFIG_T::pad_left == 0 && CONFIG_T::pad_right == 0);
+    std::cout  << "USE DEPTHWISE CONV2D SS"<< std::endl;
+    
+    // use HLS ap_shift_reg as linebuffer
+    static ap_shift_reg<data_T, (CONFIG_T::in_width+CONFIG_T::pad_left+CONFIG_T::pad_right)> linebuffer[(CONFIG_T::filt_height)-1][CONFIG_T::n_chan];
+    // #pragma HLS ARRAY_RESHAPE variable=layer_in_row complete dim=2
+    
+    // use array as linebuffer
+    //static data_T linebuffer[CONFIG_T::in_width+CONFIG_T::pad_left+CONFIG_T::pad_right][(CONFIG_T::filt_height)-1][CONFIG_T::n_chan];
+	//#pragma HLS ARRAY_PARTITION variable=linebuffer complete
+    
+    data_T tmpdata[CONFIG_T::n_chan];
+    // #pragma HLS ARRAY_RESHAPE variable=tmpdata complete
+
+    static data_T layer_in[CONFIG_T::filt_height*CONFIG_T::filt_width*CONFIG_T::n_chan];
+    // #pragma HLS ARRAY_RESHAPE variable=layer_in complete
+
+    res_T layer_out[CONFIG_T::n_filt];
+    // #pragma HLS ARRAY_RESHAPE variable=layer_out complete dim=0
+    
+	res_T out_data;
+	
+	
+	// Thresholds
+    const static int lShiftX = CONFIG_T::filt_width - 1;
+    const static int lShiftY = CONFIG_T::filt_height - 1;
+
+    // Counters
+    static int pX = 0; // Pixel X
+    static int pY = 0; // Pixel Y
+
+    static int sX = 0; // Stride X
+    static int sY = 0; // Stride Y
+	
+    Conv2dWHLoop:
+    for (unsigned i = 0; i < CONFIG_T::in_height * CONFIG_T::in_width; i++) {
+        Conv2dChanLoop:
+        for(int i1 = 0; i1 < CONFIG_T::n_chan; i1++) { 
+        // #pragma HLS UNROLL
+            tmpdata[i1] = data.read();
+        }
+        
+        // Perform LineBuffer
+		nnet::cnnshift_apshiftreg<data_T,res_T,  CONFIG_T>(tmpdata, linebuffer, layer_in);
+        //nnet::cnnshift_arr<data_T,res_T,  CONFIG_T>(tmpdata, linebuffer, layer_in);
+        
+		
+		// Check to see if we have a full kernel
+		if ( (sX - lShiftX) == 0 && (sY - lShiftY) == 0 && pY > lShiftY - 1 && pX > lShiftX - 1) {
+			
+			// Dense multiply
+			#pragma HLS INLINE region
+			
+				depthwise_ss_product<data_T,res_T, typename CONFIG_T::mult_config>(layer_in, layer_out, weights, biases);
+			
+
+			// Write output to stream when output ready
+            Con2dCastLoop:
+			for (unsigned i_ic = 0; i_ic < CONFIG_T::n_filt; i_ic++) {
+				// #pragma HLS UNROLL
+				out_data = layer_out[i_ic];
+				res.write(out_data);
+			}		
+		}
+
+		// Counter Housekeeping
+		if (pX + 1 == CONFIG_T::in_width)  // Includes padding, end of line (padded)
+		{
+			pX = 0; 
+			sX = 0;
+			if (pY + 1 == CONFIG_T::in_height) {  // Reached bottom of image
+				pY = 0; 
+				sY = 0;
+			} else {
+				pY = pY + 1;
+				// Update stride (threshold) ? subtract stride : increment stride
+				sY = ((sY - lShiftY) == 0) ? sY - CONFIG_T::stride_height + 1 : sY + 1; 
+			}
+		} else {
+			pX = pX + 1;
+			// Update stride (threshold) ? subtract stride : increment stride
+			sX = ((sX - lShiftX) == 0) ? sX - CONFIG_T::stride_width + 1 : sX + 1; 
+		}
+	}
+}
+    
 }
 #endif
